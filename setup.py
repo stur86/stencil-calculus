6e5ebbd85a8d205817dd3f593c8742d3ddf6414c
stur86 <simonesturniolo@gmail.com>
2017-08-20 08:00:10 +0100

    Created basic structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Stencyl Calculus',
+      version='0.5',
+      description='A small set of Python functions for finite differences calculus with given stencils',
+      author='Simone Sturniolo',
+      author_email='simonesturniolo@gmail.com',
+      url='https://github.com/stur86/stencil-calculus',
+      packages=['stencils'],
+      install_requites=[
+        'numpy'
+      ]
+     )
